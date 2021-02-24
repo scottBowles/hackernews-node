582c964717099f6e5afbe45628dec8b428c42041
scottBowles <shbowles@gmail.com>
2021-02-24 01:09:42 -0500

    Sketch out basic server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+
+// 1
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => null,
+  },
+};
+
+// 3
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
